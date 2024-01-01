12917172092abadd379b98eff3b0d2ac3efc3806
Avibtb <avi.singh.iit01@gmail.com>
2024-01-02 00:49:13 +0530

    SpringBoot boilerplate code.

---
+++ src/main/java/com/avinash/hotel/LuxuryHotelApplication.java
@@ -0,0 +1,13 @@
+package com.avinash.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxuryHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxuryHotelApplication.class, args);
+	}
+
+}
